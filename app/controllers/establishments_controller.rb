9f18c22481fb1b50c608fd5b3b7c78878faf463b
vando <vandorramos@gmail.com>
2024-10-22 18:28:54 -0300

    User registers an establishment successfully

--- app/controllers/establishments_controller.rb
+++ app/controllers/establishments_controller.rb
@@ -4,6 +4,21 @@ class EstablishmentsController < ApplicationController
   def show
   end
 
+  def new
+    @establishment = Establishment.new
+  end
+
+  def create
+    @establishment = current_user.build_establishment(establishment_params)
+
+    if @establishment.save
+      redirect_to establishment_path(@establishment), notice: 'Establishment successfully registered'
+    else
+      flash.now.alert = 'Unable to register establishment'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_order_and_check_user
@@ -12,5 +27,10 @@ class EstablishmentsController < ApplicationController
     if @establishment.user != current_user
       return redirect_to root_path, alert: 'You do not have access to other establishments'
     end
+  end
+
+  def establishment_params
+    params.require(:establishment).permit(:corporate_name, :brand_name, :cnpj, :address, :number, :neighborhood,
+                                          :city, :state, :zip_code, :phone_number, :email, :code)
   end
 end
--- spec/system/establishments/user_registers_establishment_spec.rb
+++ spec/system/establishments/user_registers_establishment_spec.rb
@@ -1,28 +1,56 @@
 require 'rails_helper'
 
 describe 'User registers an establishment' do
-  # it 'after authenticates' do
-  #   user = User.create!(name: 'James', last_name: 'Bond', identification_number: CPF.generate, email: 'bond@email.com',
-  #                       password: '123456abcdef', password_confirmation: '123456abcdef')
+  it 'after authenticated' do
+    user = User.create!(name: 'James', last_name: 'Bond', identification_number: CPF.generate, email: 'bond@email.com',
+                        password: '123456abcdef', password_confirmation: '123456abcdef')
 
-  #   login_as(user)
-  #   visit(root_path)
+    login_as(user)
+    visit(root_path)
 
-  #   expect(current_path).to eq(new_establishment_path)
-  #   expect(page).to have_field('Register Establishment')
-  #   expect(page).to have_field('Corporate name')
-  #   expect(page).to have_field('Brand name')
-  #   expect(page).to have_field('Cnpj')
-  #   expect(page).to have_field('Address')
-  #   expect(page).to have_field('Neighborhood')
-  #   expect(page).to have_field('City')
-  #   expect(page).to have_field('State')
-  #   expect(page).to have_field('Zip code')
-  #   expect(page).to have_field('Phone number')
-  #   expect(page).to have_field('Email')
-  #   expect(page).to have_button('Create Establishment')
-  # end
+    expect(current_path).to eq(new_establishment_path)
+    expect(page).to have_content('Please register your establishment')
+    expect(page).to have_field('Corporate name')
+    expect(page).to have_field('Brand name')
+    expect(page).to have_field('Cnpj')
+    expect(page).to have_field('Address')
+    expect(page).to have_field('Neighborhood')
+    expect(page).to have_field('City')
+    expect(page).to have_field('State')
+    expect(page).to have_field('Zip code')
+    expect(page).to have_field('Phone number')
+    expect(page).to have_field('Email')
+    expect(page).to have_button('Save')
+  end
 
-  # it 'successfully' do
-  # end
+  it 'successfully' do
+    user = User.create!(name: 'James', last_name: 'Bond', identification_number: CPF.generate, email: 'bond@email.com',
+                        password: '123456abcdef', password_confirmation: '123456abcdef')
+
+    cnpj = CNPJ.generate
+
+    login_as(user)
+    visit(root_path)
+    fill_in 'Corporate name', with: 'Giraffas Brasil S.A.'
+    fill_in 'Brand name', with: 'Giraffas'
+    fill_in 'Cnpj', with: cnpj
+    fill_in 'Address', with: 'Rua Comercial Sul'
+    fill_in 'Number', with: '123'
+    fill_in 'Neighborhood', with: 'Asa Sul'
+    fill_in 'City', with: 'Brasília'
+    fill_in 'State', with: 'DF'
+    fill_in 'Zip code', with: '70300-902'
+    fill_in 'Phone number', with: '2198765432'
+    fill_in 'Email', with: 'contato@giraffas.com.br'
+    click_on 'Save'
+
+    expect(page).to have_content('Establishment successfully registered')
+    expect(page).to have_content('My Establishment')
+    expect(page).to have_content('Giraffas Brasil S.A.')
+    expect(page).to have_content('Giraffas')
+    expect(page).to have_content(cnpj)
+    expect(page).to have_content('Rua Comercial Sul, 123 - Asa Sul - Brasília - DF - CEP: 70300-902')
+    expect(page).to have_content('2198765432')
+    expect(page).to have_content('contato@giraffas.com.br')
+  end
 end
