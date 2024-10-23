1a3e08190e44f5c07a02e119e0fe11563c2ddd97
vando <vandorramos@gmail.com>
2024-10-23 17:18:24 -0300

    There is no operating hours registered

--- spec/system/operating_hours/user_views_operating_hours_spec.rb
+++ spec/system/operating_hours/user_views_operating_hours_spec.rb
@@ -18,7 +18,7 @@ describe "User views the establishment's operating hours" do
                                   state: 'DF', zip_code: '70300-902', phone_number: '2198765432', email: 'contato@giraffas.com.br')
 
     OperatingHour.day_of_weeks.keys.each do |day|
-      OperatingHour.create!(establishment: estab, day_of_week: day, opening_time: '10:00', closing_time: '17:00', status: 'opened')
+      OperatingHour.create!(establishment: estab, day_of_week: day, opening_time: '10:00', closing_time: '17:00')
     end
 
     login_as(user)
@@ -42,4 +42,44 @@ describe "User views the establishment's operating hours" do
     expect(page).to have_content('Saturday')
     expect(page).to have_content('10:00 - 17:00')
   end
+
+  it 'and there is no operating hours registered' do
+    user = User.create!(name: 'James', last_name: 'Bond', identification_number: CPF.generate, email: 'bond@email.com',
+                        password: '123456abcdef', password_confirmation: '123456abcdef')
+
+    estab = Establishment.create!(user: user, corporate_name: 'Giraffas Brasil S.A.', brand_name: 'Giraffas', cnpj: CNPJ.generate,
+                                  address: 'Rua Comercial Sul', number: '123', neighborhood: 'Asa Sul', city: 'Brasília',
+                                  state: 'DF', zip_code: '70300-902', phone_number: '2198765432', email: 'contato@giraffas.com.br')
+
+    login_as(user)
+    visit(root_path)
+    click_on('Operating Hours')
+
+    expect(current_path).to eq(operating_hours_path)
+    expect(page).to have_content('Operating Hours')
+    expect(page).to have_content('There is no operating hours registered')
+  end
+
+  # it 'and updates the status of an operating hour to closed' do
+  #   user = User.create!(name: 'James', last_name: 'Bond', identification_number: CPF.generate, email: 'bond@email.com',
+  #                       password: '123456abcdef', password_confirmation: '123456abcdef')
+
+  #   estab = Establishment.create!(user: user, corporate_name: 'Giraffas Brasil S.A.', brand_name: 'Giraffas', cnpj: CNPJ.generate,
+  #                                 address: 'Rua Comercial Sul', number: '123', neighborhood: 'Asa Sul', city: 'Brasília',
+  #                                 state: 'DF', zip_code: '70300-902', phone_number: '2198765432', email: 'contato@giraffas.com.br')
+
+  #   OperatingHour.day_of_weeks.keys.each do |day|
+  #     OperatingHour.create!(establishment: estab, day_of_week: day, opening_time: '10:00', closing_time: '17:00', status: 'opened')
+  #   end
+
+  #   login_as(user)
+  #   visit(root_path)
+  #   click_on('Operating Hours')
+  #   click_on('Sunday')
+
+  #   expect(current_path).to eq(operating_hours_path)
+  #   expect(page).to have_content('Operating Hours')
+  #   expect(page).to have_content('Sunday')
+  #   expect(page).to have_content('closed')
+  # end
 end
