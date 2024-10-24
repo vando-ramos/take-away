283d9a8b0abfd377ef0c2cebc2eba8b490fed535
vando <vandorramos@gmail.com>
2024-10-24 18:00:14 -0300

    User edits the operating hours successfully

--- app/controllers/operating_hours_controller.rb
+++ app/controllers/operating_hours_controller.rb
@@ -1,25 +1,38 @@
 class OperatingHoursController < ApplicationController
   before_action :set_establishment
+  before_action :set_operating_hour, only: %i[show edit update]
 
   def index
-    # @operating_hours = OperatingHour.all
     @operating_hours = @establishment.operating_hours
   end
 
+  def show
+  end
+
   def new
-    # @operating_hour = @establishment.operating_hours.new
     @operating_hour = @establishment.operating_hours.build
   end
 
   def create
-    # @operating_hour = @establishment.operating_hours.new(operating_hour_params)
     @operating_hour = @establishment.operating_hours.build(operating_hour_params)
 
     if @operating_hour.save
       redirect_to establishment_operating_hours_path(@establishment.id), notice: 'Operating hour successfully registered'
     else
       flash.now.alert = 'Unable to register operating hour'
-      render :new
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @operating_hour.update(operating_hour_params)
+      redirect_to establishment_operating_hours_path(@establishment.id), notice: 'Operating hour successfully updated'
+    else
+      flash.now.alert = 'Unable to update the operating hour'
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -27,6 +40,10 @@ class OperatingHoursController < ApplicationController
 
   def set_establishment
     @establishment = Establishment.find(params[:establishment_id])
+  end
+
+  def set_operating_hour
+    @operating_hour = OperatingHour.find(params[:id])
   end
 
   def operating_hour_params
---
+++ spec/system/operating_hours/user_edits_operating_hours_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+describe 'User edits the operating hours' do
+  it 'if authenticated' do
+    visit(root_path)
+
+    expect(current_path).to eq(new_user_session_path)
+    expect(page).to have_field('Email')
+    expect(page).to have_field('Password')
+  end
+
+  it 'from the menu' do
+    user = User.create!(name: 'James', last_name: 'Bond', identification_number: CPF.generate, email: 'bond@email.com',
+                        password: '123456abcdef', password_confirmation: '123456abcdef')
+
+    estab = Establishment.create!(user: user, corporate_name: 'Giraffas Brasil S.A.', brand_name: 'Giraffas',
+                                  cnpj: CNPJ.generate, address: 'Rua Comercial Sul', number: '123',
+                                  neighborhood: 'Asa Sul', city: 'Brasília', state: 'DF', zip_code: '70300-902', phone_number: '2198765432', email: 'contato@giraffas.com.br')
+
+    OperatingHour.day_of_weeks.keys.each do |day|
+      OperatingHour.create!(establishment: estab, day_of_week: day, opening_time: '08:00', closing_time: '17:00', status: 'opened')
+    end
+
+    login_as(user)
+    visit(root_path)
+    click_on('Operating Hours')
+    click_on('Sunday')
+    click_on('Edit')
+
+    expect(page).to have_content('Edit Sunday')
+    expect(page).to have_select('Day of week', with_options: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'])
+    expect(page).to have_select('operating_hour_opening_time_4i', selected: '08')
+    expect(page).to have_select('operating_hour_opening_time_5i', selected: '00')
+    expect(page).to have_select('operating_hour_closing_time_4i', selected: '17')
+    expect(page).to have_select('operating_hour_closing_time_5i', selected: '00')
+    expect(page).to have_select('Status', with_options: ['Opened', 'Closed'])
+    expect(page).to have_button('Update Operating hour')
+  end
+
+  it 'successfully' do
+    user = User.create!(name: 'James', last_name: 'Bond', identification_number: CPF.generate, email: 'bond@email.com',
+                        password: '123456abcdef', password_confirmation: '123456abcdef')
+
+    estab = Establishment.create!(user: user, corporate_name: 'Giraffas Brasil S.A.', brand_name: 'Giraffas',
+                                  cnpj: CNPJ.generate, address: 'Rua Comercial Sul', number: '123',
+                                  neighborhood: 'Asa Sul', city: 'Brasília', state: 'DF', zip_code: '70300-902', phone_number: '2198765432', email: 'contato@giraffas.com.br')
+
+    OperatingHour.day_of_weeks.keys.each do |day|
+      OperatingHour.create!(establishment: estab, day_of_week: day, opening_time: '08:00', closing_time: '17:00', status: 'opened')
+    end
+
+    login_as(user)
+    visit(root_path)
+    click_on('Operating Hours')
+    click_on('Sunday')
+    click_on('Edit')
+    select '09', from: 'operating_hour_opening_time_4i'
+    select '30', from: 'operating_hour_opening_time_5i'
+    select '16', from: 'operating_hour_closing_time_4i'
+    select '30', from: 'operating_hour_closing_time_5i'
+    click_on('Update Operating hour')
+
+    expect(current_path).to eq(establishment_operating_hours_path(estab.id))
+    expect(page).to have_content('Operating hour successfully updated')
+    expect(page).to have_content('Sunday')
+    expect(page).to have_content('09:30 - 16:30')
+  end
+end
