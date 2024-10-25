64afbdd125ccb469fd7ae5d433b41fae29097237
vando <vandorramos@gmail.com>
2024-10-25 12:00:28 -0300

    User edits dishes and all fields are mandatory

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe Dish, type: :model do
-  
+  describe '#valid' do
+    it "Name can't be blank" do
+      user = User.create!(name: 'James', last_name: 'Bond', identification_number: CPF.generate,
+                          email: 'bond@email.com', password: '123456abcdef', password_confirmation: '123456abcdef')
+
+      estab = Establishment.create!(user: user, corporate_name: 'Giraffas Brasil S.A.', brand_name: 'Giraffas',
+                                    cnpj: CNPJ.generate, address: 'Rua Comercial Sul', number: '123',
+                                    neighborhood: 'Asa Sul', city: 'Brasília', state: 'DF', zip_code: '70300-902', phone_number: '2198765432', email: 'contato@giraffas.com.br')
+
+      dish = Dish.new(establishment: estab, name: '',
+                        description: 'Pizza com molho de tomate, queijo, calabresa e orégano',
+                        calories: 265,
+                        image: fixture_file_upload(Rails.root.join('spec/fixtures/files/pizza-calabresa.jpg'), 'image/jpg'))
+
+      expect(dish.valid?).to eq false
+    end
+
+    it "Description can't be blank" do
+      user = User.create!(name: 'James', last_name: 'Bond', identification_number: CPF.generate,
+                          email: 'bond@email.com', password: '123456abcdef', password_confirmation: '123456abcdef')
+
+      estab = Establishment.create!(user: user, corporate_name: 'Giraffas Brasil S.A.', brand_name: 'Giraffas',
+                                    cnpj: CNPJ.generate, address: 'Rua Comercial Sul', number: '123',
+                                    neighborhood: 'Asa Sul', city: 'Brasília', state: 'DF', zip_code: '70300-902', phone_number: '2198765432', email: 'contato@giraffas.com.br')
+
+      dish = Dish.new(establishment: estab, name: 'Pizza de Calabresa',
+                        description: '',
+                        calories: 265,
+                        image: fixture_file_upload(Rails.root.join('spec/fixtures/files/pizza-calabresa.jpg'), 'image/jpg'))
+
+      expect(dish.valid?).to eq false
+    end
+
+    it "Calories can't be blank" do
+      user = User.create!(name: 'James', last_name: 'Bond', identification_number: CPF.generate,
+                          email: 'bond@email.com', password: '123456abcdef', password_confirmation: '123456abcdef')
+
+      estab = Establishment.create!(user: user, corporate_name: 'Giraffas Brasil S.A.', brand_name: 'Giraffas',
+                                    cnpj: CNPJ.generate, address: 'Rua Comercial Sul', number: '123',
+                                    neighborhood: 'Asa Sul', city: 'Brasília', state: 'DF', zip_code: '70300-902', phone_number: '2198765432', email: 'contato@giraffas.com.br')
+
+      dish = Dish.new(establishment: estab, name: 'Pizza de Calabresa',
+                        description: 'Pizza com molho de tomate, queijo, calabresa e orégano',
+                        calories: '',
+                        image: fixture_file_upload(Rails.root.join('spec/fixtures/files/pizza-calabresa.jpg'), 'image/jpg'))
+
+      expect(dish.valid?).to eq false
+    end
+
+    it "Image can't be blank" do
+      user = User.create!(name: 'James', last_name: 'Bond', identification_number: CPF.generate,
+                          email: 'bond@email.com', password: '123456abcdef', password_confirmation: '123456abcdef')
+
+      estab = Establishment.create!(user: user, corporate_name: 'Giraffas Brasil S.A.', brand_name: 'Giraffas',
+                                    cnpj: CNPJ.generate, address: 'Rua Comercial Sul', number: '123',
+                                    neighborhood: 'Asa Sul', city: 'Brasília', state: 'DF', zip_code: '70300-902', phone_number: '2198765432', email: 'contato@giraffas.com.br')
+
+      dish = Dish.new(establishment: estab, name: 'Pizza de Calabresa',
+                        description: 'Pizza com molho de tomate, queijo, calabresa e orégano',
+                        calories: 265,
+                        image: '')
+
+      expect(dish.valid?).to eq false
+    end
+  end
 end
--- spec/system/dishes/user_edits_dishes_spec.rb
+++ spec/system/dishes/user_edits_dishes_spec.rb
@@ -68,4 +68,33 @@ describe 'User edits dishes' do
     expect(page).to have_content('550 cal')
     expect(page).to have_css("img[src*='carbonara.jpg']")
   end
+
+  it 'and all fields are mandatory' do
+    user = User.create!(name: 'James', last_name: 'Bond', identification_number: CPF.generate, email: 'bond@email.com',
+                        password: '123456abcdef', password_confirmation: '123456abcdef')
+
+    estab = Establishment.create!(user: user, corporate_name: 'Giraffas Brasil S.A.', brand_name: 'Giraffas',
+                                  cnpj: CNPJ.generate, address: 'Rua Comercial Sul', number: '123',
+                                  neighborhood: 'Asa Sul', city: 'Brasília', state: 'DF', zip_code: '70300-902', phone_number: '2198765432', email: 'contato@giraffas.com.br')
+
+    dish = Dish.create!(establishment: estab, name: 'Pizza de Calabresa',
+                        description: 'Pizza com molho de tomate, queijo, calabresa e orégano',
+                        calories: 265,
+                        image: fixture_file_upload(Rails.root.join('spec/fixtures/files/pizza-calabresa.jpg'), 'image/jpg'))
+
+    login_as(user)
+    visit(root_path)
+    click_on('Dishes')
+    click_on('Pizza de Calabresa')
+    click_on('Edit')
+    fill_in 'Name', with: ''
+    fill_in 'Description', with: ''
+    fill_in 'Calories', with: ''
+    click_on('Update Dish')
+
+    expect(page).to have_content('Unable to update dish')
+    expect(page).to have_content("Name can't be blank")
+    expect(page).to have_content("Calories can't be blank")
+    expect(page).to have_content("Description can't be blank")
+  end
 end
